2c58d206cb15cc1fd78f014e2f837d3ce279c823
Joe Pea <joe@lume.io>
2025-11-20 22:06:06 -0800

    update editorconfig

--- src/decorators/memo.ts
+++ src/decorators/memo.ts
@@ -1,3 +1,11 @@
-// TODO
+// export function memo(
+// 	value: unknown,
+// 	context:
+// 		| ClassFieldDecoratorContext
+// 		| ClassGetterDecoratorContext
+// 		// | ClassSetterDecoratorContext // memos are read-only
+// 		| ClassAccessorDecoratorContext,
+// ) {
+// 	const {kind, name, metadata} = context
 
-export {}
+// }
